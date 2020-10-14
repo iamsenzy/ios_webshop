02dd39ca201eba32c63fc68b01eda3b6a60da7ae
Geszti Bence <geszti.bence@codeyard.eu>
2020-10-14 21:47:53 +0200

    BaseNavigation Controller

--- Webshop/Common/BaseClasses/BaseNavigationController.swift
+++ Webshop/Common/BaseClasses/BaseNavigationController.swift
@@ -12,9 +12,16 @@ class BaseNavigationController: UINavigationController {
     
     init() {
         super.init(nibName: nil, bundle: nil)
+        setup()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
     }
 }
