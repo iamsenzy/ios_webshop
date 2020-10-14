6f24c718f44dc8c4754df14fb183a6ec841b1eac
Geszti Bence <geszti.bence@codeyard.eu>
2020-10-14 21:47:15 +0200

    Init Colors

---
+++ Webshop/Application/Colors.swift
@@ -0,0 +1,28 @@
+//
+//  Colors.swift
+//  Webshop
+//
+//  Created by Geszti Bence on 2020. 10. 14..
+//  Copyright © 2020. GesBen. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let lightGreen = UIColor(0x55efc4)
+    static let turkish = UIColor(0x81ecec)
+    static let greenBlue = UIColor(0x74b9ff)
+    static let lightPurple = UIColor(0xa29bfe)
+    static let purple = UIColor(0x6c5ce7)
+    static let blue = UIColor(0x0984e3)
+    static let lightYellow = UIColor(0xffeaa7)
+    static let lightOrange = UIColor(0xfab1a0)
+    static let lightRed = UIColor(0xff7675)
+    static let yellow = UIColor(0xfdcb6e)
+    static let orange = UIColor(0xe17055)
+    static let red = UIColor(0xd63031)
+    static let darkGray = UIColor(0x2d3436)
+    static let gray = UIColor(0x636e72)
+    static let lightGray = UIColor(0xb2bec3)
+    static let softGray = UIColor(0xdfe6e9)
+}
